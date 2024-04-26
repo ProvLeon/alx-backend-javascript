65a9c575e3d0c8b3e29bf9396c6736a6e76f857d
ProvLeon <providence.leonard@gmail.com>
2024-04-26 11:44:04 +0000

    added an indent

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,12 +2,12 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return createUser()
-  .then(({firstName, lastName }) => {
-    uploadPhoto()
-    .then(({ body }) => {
-      console.log(`${body} ${firstName} ${lastName}`);
+    .then(({firstName, lastName }) => {
+      uploadPhoto()
+      .then(({ body }) => {
+        console.log(`${body} ${firstName} ${lastName}`);
+      })
+      .catch(() => console.log('Signup system offline'));
     })
-    .catch(() => console.log('Signup system offline'));
-  })
-    .catch(() => console.log('Signup system offline'));
+      .catch(() => console.log('Signup system offline'));
 }
