23b68270c07004d8525665e336c1262e064249d2
ProvLeon <providence.leonard@gmail.com>
2024-04-26 11:44:55 +0000

    completed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,10 +4,10 @@ export default function handleProfileSignup() {
   return createUser()
     .then(({firstName, lastName }) => {
       uploadPhoto()
-      .then(({ body }) => {
-        console.log(`${body} ${firstName} ${lastName}`);
-      })
-      .catch(() => console.log('Signup system offline'));
+        .then(({ body }) => {
+          console.log(`${body} ${firstName} ${lastName}`);
+        })
+        .catch(() => console.log('Signup system offline'));
     })
-      .catch(() => console.log('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
