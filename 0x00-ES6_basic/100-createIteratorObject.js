4f9ad4b5b8ca305de6e53c4223bb74ec70c99770
ProvLeon <providence.leonard@gmail.com>
2024-04-26 10:17:20 +0000

    done

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,14 +2,14 @@ const createIteratorObject = (report) => {
   let employees = [];
   for (const department of Object.values(report.allEmployees)) {
     employees = employees.concat(department);
-  }
+  };
 
   return {
       *[Symbol.iterator]() {
         for (const employee of employees) {
           yield employee;
-        }
-      }
+        };
+      },
   };
 }
 
