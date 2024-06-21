cf8a1b11fc0dcdc745c07f2b5cd34b09d7ea4386
ProvLeon <providence.leonard@gmail.com>
2024-06-21 16:16:12 +0000

    done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,19 +1,36 @@
 const assert = require('assert');
-
 const calculateNumber = require('./0-calcul');
 
-describe('sum', () => {
-  it('sum of 1 and 4 should 4', () => {
-    assert.equal(calculateNumber(1, 3), 4);
+describe('calculateNumber', function() {
+  it('should return 4 when inputs are 1 and 3', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it('sum of 1 and 3.7 should 5', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
-  });
-  it('sum of 1.2 and 3.7 should be 5', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-  it('sum of 1.5 and 3.7 should be 6', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  })
-});
 
+  it('should return 5 when inputs are 1 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 when inputs are 1.2 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when inputs are 1.5 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 0 when inputs are 0.1 and 0.2', function() {
+    assert.strictEqual(calculateNumber(0.1, 0.2), 0);
+  });
+
+  it('should return 1 when inputs are 0.5 and 0.4', function() {
+    assert.strictEqual(calculateNumber(0.5, 0.4), 1);
+  });
+
+  it('should return -1 when inputs are -0.4 and -0.5', function() {
+    assert.strictEqual(calculateNumber(-0.4, -0.5), -1);
+  });
+
+  it('should return -2 when inputs are -0.6 and -1.4', function() {
+    assert.strictEqual(calculateNumber(-0.6, -1.4), -2);
+  });
+});
