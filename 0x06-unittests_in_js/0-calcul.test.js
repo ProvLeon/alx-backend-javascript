b698b76e687153538238c2746db1416e175599c3
ProvLeon <providence.leonard@gmail.com>
2024-06-21 17:26:32 +0000

    done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,6 +6,9 @@ describe('sum', () => {
   it('sum of 1 and 4 should 4', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
+  it('sum of 3.7 and 2.2 should be 6', () => {
+    assert.strictEqual(calculateNumber(3.7, 2.2), 6)
+  })
   it('sum of 1 and 3.7 should 5', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
