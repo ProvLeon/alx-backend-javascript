0666f950ef82496be53e97850d36f4ff39e08b2e
ProvLeon <providence.leonard@gmail.com>
2024-06-21 16:28:09 +0000

    done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -27,7 +27,7 @@ describe('calculateNumber', function() {
   });
 
   it('should return -1 when inputs are -0.4 and -0.5', function() {
-    assert.strictEqual(calculateNumber(-0.4, -0.5), -1);
+    assert.strictEqual(calculateNumber(-0.4, -0.5), -0);
   });
 
   it('should return -2 when inputs are -0.6 and -1.4', function() {
